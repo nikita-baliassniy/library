0386019a0a41079f6a6516851b34f93aae70e3c8
nikita.baliassniy <nikita.baliassniy@yandex.ru>
2021-08-17 16:50:07 +0300

    Books added, chandeg doc to epub as more popular

--- src/main/java/ru/geekbrains/library/configs/ModelConfig.java
+++ src/main/java/ru/geekbrains/library/configs/ModelConfig.java
@@ -24,7 +24,6 @@ public class ModelConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-
         configureBook(modelMapper);
         configureUser(modelMapper);
         return modelMapper;
