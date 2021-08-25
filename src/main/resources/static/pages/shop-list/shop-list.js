eb1ca3a20f857597baa79fcf41b099bb685113e0
Egor.Vasyakov@waveaccess.ru <Egor.Vasyakov@waveaccess.ru>
2021-08-25 19:42:55 +0300

    Обновлено оформление и пагинация в каталоге

--- src/main/resources/static/pages/shop-list/shop-list.js
+++ src/main/resources/static/pages/shop-list/shop-list.js
@@ -1,8 +1,18 @@
 angular.module('library').controller('shopListController', function ($scope, $http, API_SERVER) {
 
+    let pageSelector = document.querySelector('.index-selector select');
+    pageSelector.options.selectedIndex = 0;
+    $scope.size = pageSelector.options[pageSelector.options.selectedIndex.valueOf()].value;
+    if ($scope.genre == null) $scope.genre = [];
+
+    pageSelector.addEventListener('change', function () {
+        let selectedIndex = pageSelector.options.selectedIndex.valueOf();
+        $scope.size = pageSelector.options[selectedIndex].value;
+        $scope.findAllProducts();
+    });
+
     $scope.fillTable = function (pageIndex) {
         $scope.findAllProducts(pageIndex);
-        // $scope.filter && $scope.filter.id != null ? $scope.findProductById() : $scope.findAllProducts(pageIndex);
     };
 
     $scope.findAllProducts = function (pageIndex = 1) {
@@ -13,13 +23,12 @@ angular.module('library').controller('shopListController', function ($scope, $ht
                 min: $scope.filter ? $scope.filter.min : null,
                 max: $scope.filter ? $scope.filter.max : null,
                 title: $scope.filter ? $scope.filter.title : null,
-                count: $scope.filter ? $scope.filter.size : 10,
+                count: $scope.size ? $scope.size : 9,
                 page: pageIndex
             },
         }).then(function (response) {
             $scope.ProductsPage = response.data;
             $scope.ProductsList = $scope.ProductsPage.content;
-            console.log($scope.ProductsPage)
             if ($scope.ProductsPage.empty && pageIndex !== 1) {
                 $scope.fillTable(pageIndex--);
             }
@@ -35,7 +44,6 @@ angular.module('library').controller('shopListController', function ($scope, $ht
         $http.get(API_SERVER + '/books/' + $scope.filter.id)
             .then(function (response) {
                 $scope.ProductsList = [response.data];
-                console.log($scope.ProductsList)
                 $scope.PageArray = [1];
             });
     };
@@ -46,16 +54,6 @@ angular.module('library').controller('shopListController', function ($scope, $ht
             array.push(i);
         }
         $scope.PageArray = array;
-    };
-
-    $scope.changePagination = function () {
-        let selectElement = document.getElementById("pagination").options.selectedIndex;
-        let val = document.getElementById("pagination").options[selectElement].value;
-        if ($scope.value !== val) {
-            $scope.value = val;
-            $scope.pageIndex = 1;
-            $scope.fillTable();
-        }
     };
 
     $scope.cleanFilter = function () {
